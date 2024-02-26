152b79339f13c2a5162c3dc2f76263dae0b7a696
andresvacal <77646529+andresvacal@users.noreply.github.com>
2024-02-26 12:31:55 -0500

    Pantalla inicial completada

---
+++ app/src/main/java/com/example/fotoconnect/RecyclerViewAdapter.java
@@ -0,0 +1,44 @@
+package com.example.fotoconnect;
+
+// YourRecyclerViewAdapter.java
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    // ... other methods ...
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.messageitem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    // ... other methods ...
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewMessage;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewMessage = itemView.findViewById(R.id.textViewMessage);
+        }
+    }
+}
