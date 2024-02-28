0105b3d993e3cfed578763185b8c10e8a698e54c
andresvacal <77646529+andresvacal@users.noreply.github.com>
2024-02-27 22:16:09 -0500

    Actualizacion de nombres y dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
@@ -44,6 +45,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.room:room-compiler-processing-testing:2.6.1")
     implementation ("com.google.android.material:material:1.1.0")
+    implementation ("com.android.support:multidex:2.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
