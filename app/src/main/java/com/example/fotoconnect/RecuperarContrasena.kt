c1999640052d3e4ece7f7eb62d481e791a0345b7
Kuribbo <56056695+Kuribbo@users.noreply.github.com>
2024-05-17 02:10:00 -0500

    Arreglando notis :(

--- app/src/main/java/com/example/fotoconnect/RecuperarContrasena.kt
+++ app/src/main/java/com/example/fotoconnect/RecuperarContrasena.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
+
 class RecuperarContrasena : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recuperar_contrasena)
     }
 
+    
     override fun onBackPressed() {
         // Override the back button behavior
         // If you want to go back to IniciaSesion, start its activity
